46924ccf5e07d07669d6abb191467779a42184f8
zhouzh6 <zhouzh6@asiainfo.com>
2020-10-13 15:36:45 +0800

    秒杀模块构建

---
+++ shoehome-seckill/src/main/java/com/zhou/shoehome/seckill/ShoehomeSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.shoehome.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoehomeSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoehomeSeckillApplication.class, args);
+    }
+
+}
---
+++ shoehome-seckill/src/test/java/com/zhou/shoehome/seckill/ShoehomeSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.shoehome.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoehomeSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
